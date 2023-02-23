6ffc327bf6b2f33e6e60f16a34b350e8fe536e33
김용건 (Kim Younggeon) <opensource@younggeon.kim>
2023-02-23 17:12:43 +0900

    style: [skip ci]

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,14 +13,10 @@ export default defineNuxtConfig({
     rootId: 'app',
 
     head: {
-      htmlAttrs: {
-        lang: 'ko',
-        dir: 'ltr'
-      },
       meta: [
         { charset: 'utf-8' },
-        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      ]
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+      ],
     }
   },
   css: [
@@ -77,34 +73,34 @@ export default defineNuxtConfig({
     theme: {
       colors: {
         gray: {
-          50: "#f8f9fa",
-          100: "#f1f3f5",
-          200: "#e9ecef",
-          300: "#dee2e6",
-          400: "#ced4da",
-          500: "#adb5bd",
-          600: "#868e96",
-          700: "#495057",
-          800: "#343a40",
-          900: "#212529",
+          50: '#f8f9fa',
+          100: '#f1f3f5',
+          200: '#e9ecef',
+          300: '#dee2e6',
+          400: '#ced4da',
+          500: '#adb5bd',
+          600: '#868e96',
+          700: '#495057',
+          800: '#343a40',
+          900: '#212529'
         },
+        container: {
+          maxWidth: '80rem', // max-w7xl
+          center: true, // max-auto
+          padding: {
+            DEFAULT: '1.5rem', // px6
+            lg: '2rem', // px8
+            xl: '2.5rem', // px10
+          }
+        }
       }
     },
     shortcuts: [
       // `border` 단축키
-      [
-        /^b-(.*)$/,
-        ([,p]) => `border-${p}`
-      ],
-      [
-        /^b[\d+]$/,
-        ([,n]) => `border-${n}`
-      ],
-      [
-        /^b([trbl])?-?([0-9]+)?$/,
-        ([,p,n]) => `border${p ? `-${p}` : ''}${n ? `-${n}` : ''}`
-      ],
+      [/^b-(.*)$/, ([, p]) => `border-${p}`],
+      [/^b([trbl])?-?([0-9]+)?$/, ([, p, n]) => `border${p ? `-${p}` : ''}${n ? `-${n}` : ''}`],
 
+      // 테마
       ['bg-base', 'light:bg-gray-50 dark:bg-gray-900'],
       ['bg-panel', 'light:bg-white dark:bg-gray-800'],
 
